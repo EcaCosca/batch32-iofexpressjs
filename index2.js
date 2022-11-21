9b1b8aeeb9aad8b973b48fd9d0abff7d640eb595
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-21 11:36:41 +0100

    Ex 2 done with coments and alternatives

--- index2.js
+++ index2.js
@@ -18,9 +18,16 @@ app.put('/', function(req, res){
             console.log('Sent:', fileName);
         }
     });
+
+    
 });
+
+// Susi's solution
+// app.put("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
  
 app.listen(PORT, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
 });
+
+// https://www.geeksforgeeks.org/express-js-res-sendfile-function/
